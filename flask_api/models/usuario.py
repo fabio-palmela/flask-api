4956b211132921e6210c824fb9dd6f6a3888907e
Fabio Palmela de Oliveira <fabio.oliveira@credicom.com.br>
2022-02-01 02:16:02 -0300

    Criação do model user

---
+++ flask_api/models/usuario.py
@@ -0,0 +1,33 @@
+from sql_alchemy import banco
+
+class HotelModel(banco.Model):
+    __tablename__ = 'usuarios'
+    user_id = banco.Column(banco.Integer, primary_key=True)
+    login = banco.Column(banco.String(40))
+    senha = banco.Column(banco.String(40))
+
+    def __init__(self, login, senha):
+        self.login = login
+        self.senha = senha
+
+    def json(self):
+        return {
+            'user_id': self.user_id,
+            'login': self.login,
+            'senha': self.senha
+        }
+
+    @classmethod
+    def find_user(cls, user_id):
+        user = cls.query.filter_by(user_id=user_id).first()
+        if user:
+            return user
+        return False        
+
+    def save_user(self):
+        banco.session.add(self)
+        banco.session.commit()
+
+    def remove_hotel(self):
+        banco.session.delete(self)
+        banco.session.commit()
