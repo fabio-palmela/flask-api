3d569e225d40b88e8413fa1c0aed29feeadb4b92
Fabio Palmela de Oliveira <fabio.oliveira@credicom.com.br>
2022-02-01 19:54:14 -0300

    Correção delete usuário

--- flask_api/models/hotel.py
+++ flask_api/models/hotel.py
@@ -44,10 +44,3 @@ class HotelModel(banco.Model):
     def remove_hotel(self):
         banco.session.delete(self)
         banco.session.commit()
-
-
-        # def gera_novo_hotel():
-        # novo_id = max(item['hotel_id'] for item in hoteis)
-        # dados = Hotel.argumentos.parse_args()
-        # objHotel = HotelModel(novo_id + 1, **dados)
-        # hoteis.append(objHotel.json())
--- flask_api/resources/usuario.py
+++ flask_api/resources/usuario.py
@@ -3,14 +3,14 @@ from models.usuario import UserModel
           
 class User(Resource):    
     def get(self, user_id):
-        user = UserModel.find_hotel(user_id)
+        user = UserModel.find_user(user_id)
         if (user):
             return user.json()
         else:
             return {'message': 'User not found.'}, 404
      
     def delete(self, user_id):
-        user = UserModel.find_hotel(hotel_user_idid)
+        user = UserModel.find_user(user_id)
         if user:
             try:
                 user.remove_user()
