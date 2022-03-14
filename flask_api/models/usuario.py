6e9c1d76e825a04688f71b03d6b4333dfc5dcae0
Fabio Palmela de Oliveira <fabio.oliveira@credicom.com.br>
2022-03-14 18:53:57 -0300

    Inclusão de obrigatoriedade de ativação de usuário cadastrado

--- flask_api/models/usuario.py
+++ flask_api/models/usuario.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from sql_alchemy import banco
 
 class UserModel(banco.Model):
@@ -5,16 +6,19 @@ class UserModel(banco.Model):
     user_id = banco.Column(banco.Integer, primary_key=True)
     login = banco.Column(banco.String(40))
     senha = banco.Column(banco.String(40))
+    ativado = banco.Column(banco.Boolean, default=False)
 
-    def __init__(self, login, senha):
+    def __init__(self, login, senha, ativado):
         self.login = login
         self.senha = senha
+        self.senha = ativado
 
     def json(self):
         return {
             'user_id': self.user_id,
             'login': self.login,
-            'senha': self.senha
+            'senha': self.senha,
+            'ativado': self.ativado,
         }
 
     @classmethod
